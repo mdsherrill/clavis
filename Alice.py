0c3e4f6c4ec11156231fa1033a8f6b5431180460
Matt Sherrill <matthew@sherrillfamily.net>
2023-09-21 12:55:31 -0500

    Done with AES and RSA encryptions. Moving along with testing speed.

--- Alice.py
+++ Alice.py
@@ -1,4 +1,9 @@
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.backends import default_backend
 import os, sys
 def aes_encrypt():
     # Creating public key, then writing to key.txt
@@ -22,7 +27,32 @@ def aes_encrypt():
         ctext_file.write(ciphertext)
 
 def rsa_encrypt():
-    pass
+    # Read Bob's public key from file
+    # with open('rsa_key.txt', 'rb') as key_file:
+    #     rsa_public_key = key_file.read()
+
+    with open("rsa_public_key.txt", "rb") as key_file:
+        rsa_public_key = serialization.load_pem_public_key(
+            key_file.read(),
+            backend=default_backend()
+        )
+
+    message = input("Enter the message to encrypt: ").encode()
+
+    # Create RSA Cipher
+    ciphertext = rsa_public_key.encrypt(
+        message,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+
+    with open("rsa_ciphertext.txt", 'wb') as key_file:
+        key_file.write(ciphertext)
+    print("Encryption to file complete.\n\nDerived Ciphertext:", ciphertext.hex())  # Convert to hexadecimal for easy printing
+
 
 if sys.argv[1] == '1':
     aes_encrypt()
--- Bob.py
+++ Bob.py
@@ -1,4 +1,9 @@
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.backends import default_backend
 import os, sys
 
 def aes_decrypt():
@@ -21,10 +26,58 @@ def aes_decrypt():
     print("Received Ciphertext:", ciphertext.hex())  # Print the received ciphertext in hexadecimal
     print("Decrypted message:", decrypted_message.decode())
 
+def create_rsa_key():
+    private_key = rsa.generate_private_key(
+        public_exponent=65537, # Always must be 65537 - Standard
+        key_size=2048,
+    )
+    public_key = private_key.public_key()
+
+    # Serialize the public key to PEM format
+    public_pem = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
+
+    private_pem = private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.BestAvailableEncryption(b'mypassword')
+    )
+
+    with open("rsa_public_key.txt", "wb") as key_file:
+        key_file.write(public_pem)
+    with open("rsa_private_key.txt", "wb") as key_file:
+        key_file.write(private_pem)
+
 def rsa_decrypt():
-    pass
+    # Open and read file
+    with open("rsa_private_key.txt", "rb") as key_file:
+        private_key = serialization.load_pem_private_key(
+            key_file.read(),
+            password=b'mypassword',
+        )
+
+    # Open the ciphertext file
+    with open("rsa_ciphertext.txt", 'rb') as ciph_text:
+        ciphertext = ciph_text.read()
+    print("Received ciphertext: ", ciphertext.hex())
+
+    # Decrypt using ciphertext and private key
+    plaintext = private_key.decrypt(
+        ciphertext,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+
+    print("Decryption complete.\n\nDeciphered Message: ", plaintext)
 
 if sys.argv[1] == '1':
     aes_decrypt()
+elif sys.argv[1] == 'c':
+    create_rsa_key()
 elif sys.argv[1] == '2':
     rsa_decrypt()
